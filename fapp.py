4e6b0965fc9bcb7a82c1bdaf7cc5a983e1e516f9
mh-firouzjaah <mh.firouzjah@gmail.com>
2020-10-21 13:15:36 +0330

    connecting to cnn search api

--- fapp.py
+++ fapp.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +8,16 @@ app = Flask(__name__)
 def hello_world():
     """Print 'Hello, world!' as the response body."""
     return 'Hello, world!'
+
+
+@app.route('/cnn-covid')
+def cnn():
+    """Connect to CNN search API, do the search then return the result."""
+    url = ('https://search.api.cnn.io/content?'
+           'q=covid+coronavirus&size=25&from=0&type=article')
+    json_data = requests.get(url).json()
+    data = json_data['result']
+    return render_template('cnn.html', data=data)
 
 
 if __name__ == "__main__":
